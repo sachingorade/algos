732324b5099aefbe99d077a4d4a9768f1f435c32
Sachin Gorade <sachin.gorade@gmail.com>
2020-09-22 20:28:03 +0200

    Corrected test case expectation

--- src/test/java/com/sachin/algos/ds/sums/PrefixSumTest.java
+++ src/test/java/com/sachin/algos/ds/sums/PrefixSumTest.java
@@ -18,7 +18,7 @@ class PrefixSumTest {
         assertEquals(9, new PrefixSum().getSubArraySum(new int[]{1, 2, 3, 4}, 1, 3));
         assertEquals(5, new PrefixSum().getSubArraySum(new int[]{1, 2, 3, 4, 5, 6}, 1, 2));
         assertEquals(12, new PrefixSum().getSubArraySum(new int[]{1, 2, 3, 4, 5, 6}, 2, 4));
-        assertEquals(12, new PrefixSum().getSubArraySum(new int[]{2, 3, 7, 5, 1, 3, 9}, 0, 1, true));
+        assertEquals(5, new PrefixSum().getSubArraySum(new int[]{2, 3, 7, 5, 1, 3, 9}, 0, 1, true));
     }
 
 }
